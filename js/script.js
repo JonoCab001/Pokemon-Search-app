755aee6ae419d99d9f01a4fd0d0e259d37f3e3a8
JonoCab001 <cjonathan193@gmail.com>
2025-06-20 20:10:05 +1000

    Added Functionality to get Pokemon Stats

--- js/script.js
+++ js/script.js
@@ -23,17 +23,24 @@ const searchPokedex = async () => {
         const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${userInput.value.toLowerCase()}`);
         const data = await res.json();
 
-        const { name, weight, height, id, stats, sprites, types } = data;
+        const { name, id, weight, height, types, stats, sprites } = data;
         
         pokemonName.innerHTML = name.toUpperCase();
         pokemonId.innerHTML = id;
         pokemonWeight.innerHTML = `Weight: ${weight}`;
         pokemonHeight.innerHTML = `Height: ${height}`;
         
-        pokemonImage.innerHTML = `<img src="${sprites.front_default}">`;
+        pokemonImage.innerHTML = `<img id="sprite" src="${sprites.front_default}">`;
 
         pokemonTypes.innerHTML = types.map(type => `<span>${type.type.name.toUpperCase()}</span>`).join(" ");
 
+        hp.innerHTML = stats[0].base_stat;
+        attack.innerHTML = stats[1].base_stat;
+        defense.innerHTML = stats[2].base_stat;
+        specialAttack.innerHTML = stats[3].base_stat;
+        specialDefense.innerHTML = stats[4].base_stat;
+        speed.innerHTML = stats[5].base_stat;
+        
         console.log(data);
     }
     
