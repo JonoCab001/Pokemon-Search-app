9042129a4a262f0c7ae14c93259b6a2375caa8cf
JonoCab001 <cjonathan193@gmail.com>
2025-06-20 12:40:07 +1000

    Added Functionality to search for Pokemon

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const searchButton = document.getElementById("search-button");
 // Pokemon Data
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
+const pokemonImage = document.getElementById("pokemon-image");
 const pokemonWeight = document.getElementById("weight");
 const pokemonHeight = document.getElementById("height");
 const pokemonTypes = document.getElementById("types");
@@ -15,3 +16,36 @@ const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
+
+// To get Pokemon Data
+const searchPokedex = async () => {
+    try {
+        const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${userInput.value.toLowerCase()}`);
+        const data = await res.json();
+
+        const { name, weight, height, id, stats, sprites, types } = data;
+        
+        pokemonName.innerHTML = name.toUpperCase();
+        pokemonId.innerHTML = id;
+        pokemonWeight.innerHTML = `Weight: ${weight}`;
+        pokemonHeight.innerHTML = `Height: ${height}`;
+        
+        pokemonImage.innerHTML = `<img src="${sprites.front_default}">`;
+
+        pokemonTypes.innerHTML = types.map(type => `<span>${type.type.name.toUpperCase()}</span>`).join(" ");
+
+        console.log(data);
+    }
+    
+    catch(err) {
+        alert("Pokemon wasn't found :(");
+        console.log(err.message);
+    }
+}
+
+searchButton.addEventListener('click', searchPokedex);
+userInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        searchPokedex();
+    }
+})
