ea59d6ffa0bfc247c60133b8736f739b09ee5717
JonoCab001 <cjonathan193@gmail.com>
2025-06-20 10:40:15 +1000

    Added code to read Pokemon data

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// User Input forms
+const userInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+// Pokemon Data
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+const pokemonTypes = document.getElementById("types");
+
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
